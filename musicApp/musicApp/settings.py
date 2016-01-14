c233e0aa3906c5a6a62e3232d50946d4c2a9ad4c
paulkg4u <paulkottalil20@gmail.com>
2016-01-15 01:29:30 +0530

    changed mail settings

--- musicApp/musicApp/settings.py
+++ musicApp/musicApp/settings.py
@@ -38,8 +38,6 @@ ALLOWED_HOSTS = []
 EMAIL_USE_TLS = "True"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "paulkodez@gmail.com"
-EMAIL_HOST_PASSWORD = "xxx"
 """
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
