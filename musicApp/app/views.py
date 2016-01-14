d35261923ae6d7f876d9272fc871b47a7ce6f46d
Paul <paulkottalil20@gmail.com>
2016-01-15 02:22:43 +0530

    pydub added to requirements

--- musicApp/app/views.py
+++ musicApp/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 def home(request):
-	if request.user == 'AnonymousUser':
+	if request.user != 'AnonymousUser':
 		return HttpResponseRedirect('/accounts/login/')
 	contextDi = {}
 	return render(request,'musicApp/home.html',contextDi)
