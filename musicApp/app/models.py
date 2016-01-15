e20e9f8d6aaaf254a24a3819dfe6b00da0f8cf80
Paul <paulkottalil20@gmail.com>
2016-01-15 12:19:09 +0530

    rename the files

--- musicApp/app/models.py
+++ musicApp/app/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from userprofile.models import UserProfile
 
+
+def image_path(instance,filename):
+	return 'userpics'+str(instance.fileOwner.userObject.username)+str(instance.id)
+
 class AudioFile(models.Model):
 	fileOwner = models.ForeignKey(UserProfile)
 	fileDescription = models.TextField(default = "",null  = True, blank  = True)
-	audioFile = models.FileField(upload_to = "files/")
+	audioFile = models.FileField(upload_to = image_path)
 	slugText = models.SlugField(max_length = 100, null = True, blank = True)
 	def __unicode__(self):
 		return unicode(self.id)
--- musicApp/userprofile/models.py
+++ musicApp/userprofile/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class UserProfile(models.Model):
 	userObject = models.OneToOneField(User)
 	firstName = models.CharField(max_length = "200")
